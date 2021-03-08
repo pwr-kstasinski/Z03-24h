814af0789d7025dc837c148e86426a1a650b7c81
Ernest P <epenlp@gmail.com>
2021-03-08 22:27:05 +0100

    task 1,7 done

---
+++ Lab 2/Solutions/Zad1.bat
@@ -0,0 +1,23 @@
+@echo off
+
+if "%1"=="" (
+    echo Use this function like that:
+    echo    Zad1.bat ^<extension^> ^<path^>^(optional^)
+    goto :end
+)
+
+set EXT=%1
+set PATH="."
+
+if not "%2"=="" set PATH=%2
+
+for %%i in (%PATH%/*) do (
+    if %%~xi==%EXT% (
+        echo %%i
+        set FOUND=yes
+    )
+)
+
+if not defined FOUND echo Nothing found.
+
+:end
