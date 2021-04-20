dd9be8d416904a3773fc4c34ccc358451a2808db
EP-coode <--global>
2021-04-20 14:35:26 +0200

    implement temp chart

--- Lab 7/Solutions/ts-forecast/src/App.js
+++ Lab 7/Solutions/ts-forecast/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import DailyForecastTile from "./Components/DayForecastTile"
+import TemperatureChart from "./Components/TemperatureChart"
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <input type="text" placeholder="nazwa miasta"></input>
+      <button>Szukaj</button>
       <DailyForecastTile></DailyForecastTile>
+      <TemperatureChart temperatures = {[0, 2, 3, 7, 8, 10, 12, 6, 3, -2, -4]}></TemperatureChart>
     </div>
   );
 }
