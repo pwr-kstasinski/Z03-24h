850e762b10a322fac29ffb53f9240087d9f3b786
Patryk Stępień <p.stepien@poczta.pl>
2021-04-25 19:49:48 +0200

    Add solution for lab7

---
+++ Lab 7/Solutions/apiclient/api/api.ts
@@ -0,0 +1,7 @@
+export * from './class120HourHourlyForecast.service';
+import { Class120HourHourlyForecastService } from './class120HourHourlyForecast.service';
+export * from './class120HourHourlyForecast.serviceInterface'
+export * from './class16DayDailyForecast.service';
+import { Class16DayDailyForecastService } from './class16DayDailyForecast.service';
+export * from './class16DayDailyForecast.serviceInterface'
+export const APIS = [Class120HourHourlyForecastService, Class16DayDailyForecastService];
